22f51f6f7d56f0d610fc49beccbafceb6acdf1e1
johnyob <johnyob132@gmail.com>
2018-08-03 13:55:53 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+packages = [
+    "ascii_table"
+]
+
+setuptools.setup(
+    name="ascii-table",
+    version="0.0.1",
+    author="Alistair O'Brien",
+    author_email="alistair.o'brien@ellesmere.com",
+    description="An simple json to ascii table Python package.",
+    url="https://github.com/johnyob/ascii-table",
+    packages=packages
+)
