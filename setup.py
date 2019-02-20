07cc8f23d08d2909bafa1ccd93122f4c1804f982
Alistair <johnyob132@gmail.com>
2019-02-20 19:17:02 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     name="ascii-table",
     version="0.1.0",
     author="Alistair O'Brien",
-    author_email="alistair.o'brien@ellesmere.com",
+    author_email="alistair@duneroot.co.uk",
     description="An simple json to ascii table Python package.",
     url="https://github.com/johnyob/ascii-table",
     packages=packages
